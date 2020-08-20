93729857a43a0ed8dc87e4a8287e86bef00ba168
careerdimasprayoga <career.dimasprayoga@gmail.com>
2020-08-20 20:29:54 +0700

    adding files

---
+++ src/components/_base/Navbar.vue
@@ -0,0 +1,52 @@
+<template>
+  <!-- Navbar -->
+  <div class="d-flex">
+    <section>
+      <b-container fluid>
+        <b-row>
+          <b-col cols="8">
+            <div class="navbarOne">
+              tes
+            </div>
+          </b-col>
+          <b-col cols="4">
+            <div class="navbarTwo">
+              tes
+            </div>
+          </b-col>
+        </b-row>
+      </b-container>
+      <div>
+        <router-link to="/">Home</router-link> |
+        <router-link to="/axios">Axios</router-link> |
+        <router-link to="lifecycle">Lifecycle</router-link>
+      </div>
+    </section>
+  </div>
+  <!-- End Navbar -->
+</template>
+
+<style scoped>
+.container-fluid {
+  padding-right: 0;
+  padding-left: 0;
+  margin-right: auto;
+  margin-left: auto;
+}
+.navbarOne {
+  height: 70px;
+  background-color: #ffffff;
+  box-shadow: 1px 1px 10px grey;
+  z-index: 1;
+  border-left: #cecece 1px solid;
+  border-right: #cecece 1px solid;
+}
+.navbarTwo {
+  height: 70px;
+  background-color: #ffffff;
+  box-shadow: 1px 1px 10px grey;
+  z-index: 1;
+  border-left: #cecece 1px solid;
+  border-right: #cecece 1px solid;
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,39 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <!-- Navbar -->
+  <section>
+    <b-container fluid>
+      <b-row>
+        <b-col cols="8">
+          <div class="navbarOne">
+            tes
+          </div>
+        </b-col>
+        <b-col cols="4">
+          <div class="navbarTwo">
+            tesa
+          </div>
+        </b-col>
+      </b-row>
+    </b-container>
+  </section>
+  <!-- End Navbar -->
 </template>
 
-<script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
-export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
+<style scoped>
+.navbarOne {
+  height: 70px;
+  background-color: #ffffff;
+  box-shadow: 1px 1px 10px grey;
+  z-index: 1;
+  border-left: #cecece 1px solid;
+  border-right: #cecece 1px solid;
 }
-</script>
+.navbarTwo {
+  height: 70px;
+  background-color: #ffffff;
+  box-shadow: 1px 1px 10px grey;
+  z-index: 1;
+  border-left: #cecece 1px solid;
+  border-right: #cecece 1px solid;
+}
+</style>
